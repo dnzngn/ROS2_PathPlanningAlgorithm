56c50621927f28eebb52d67a467474c50ab01565
Deniz Çetin <38467947+dnzngn@users.noreply.github.com>
2023-07-05 10:39:02 +0300

    Update astar_opt.h
    - continue'dan kaçınıldı.

--- Ros2/cpp_pubsub/src/astar/astar_opt.h
+++ Ros2/cpp_pubsub/src/astar/astar_opt.h
@@ -1,51 +1,8 @@
 //
-// Created by atakan on 03.07.2023.
+// Created by deniz on 05.07.2023.
 //
-#ifndef UNTITLED9_ASTAR_NEW_H
-#define UNTITLED9_ASTAR_NEW_H
 
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <algorithm>
-#include <queue>
-#include <unordered_set>
-class Node_s {
-public:
-    int x, y;
-    double g, h, f;
-    bool obstacle;
-    Node_s* parent;
-    std::vector<std::vector<Node_s*>>& grid;
-
-    Node_s(int x, int y, std::vector<std::vector<Node_s*>>& grid)
-            : x(x), y(y), g(0), h(0), f(0), obstacle(false), parent(nullptr), grid(grid) {
-    }
-
-    ~Node_s() {
-        delete parent;
-    }
-
-    double squaredDistance(Node_s* other) {
-        return std::pow(x - other->x, 2) + std::pow(y - other->y, 2);
-    }
-
-    Node_s* getNeighbor(int dx, int dy) {
-        int newX = x + dx;
-        int newY = y + dy;
-        if (newX >= 0 && newX < grid.size() && newY >= 0 && newY < grid[newX].size()) {
-            return grid[newX][newY];
-        }
-        return nullptr;
-    }
-};
-
-class CompareNodes {
-public:
-    bool operator()(const Node_s* a, const Node_s* b) const {
-        return a->f > b->f;
-    }
-};
+#include "astar_opt.h"
 
 std::vector<Node_s*> AStar(Node_s* startNode, Node_s* endNode, std::vector<std::vector<Node_s*>>& grid) {
     std::vector<Node_s*> path;
@@ -79,26 +36,23 @@ std::vector<Node_s*> AStar(Node_s* startNode, Node_s* endNode, std::vector<std::
             int dy = dyValues[i];
 
             Node_s* neighbor = currentNode->getNeighbor(dx, dy);
-            if (neighbor == nullptr || neighbor->obstacle || closedSet.count(neighbor) > 0) {
-                continue;
-            }
 
-            double gScore = currentNode->g + currentNode->squaredDistance(neighbor);
-            double hScore = neighbor->squaredDistance(endNode);
-            double fScore = gScore + hScore;
+            if (!(neighbor == nullptr || neighbor->obstacle || closedSet.count(neighbor) > 0)) {
+                double gScore = currentNode->g + currentNode->squaredDistance(neighbor);
+                double hScore = neighbor->squaredDistance(endNode);
+                double fScore = gScore + hScore;
 
-            if (neighbor->parent == nullptr || fScore < neighbor->f) {
-                neighbor->parent = currentNode;
-                neighbor->g = gScore;
-                neighbor->h = hScore;
-                neighbor->f = fScore;
+                if (neighbor->parent == nullptr || fScore < neighbor->f) {
+                    neighbor->parent = currentNode;
+                    neighbor->g = gScore;
+                    neighbor->h = hScore;
+                    neighbor->f = fScore;
 
-                openList.push(neighbor);
+                    openList.push(neighbor);
+                }
             }
         }
     }
 
     return path;
 }
-
-#endif //UNTITLED9_ASTAR_NEW_H
